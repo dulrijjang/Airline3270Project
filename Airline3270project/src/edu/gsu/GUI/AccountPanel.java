b7f819ad03c409fd8992b7de17cc44cee8d7d344
영택 <@DESKTOP-8SKBATG.attlocal.net>
2020-12-25 20:16:01 -0500

    can't fix db

--- Airline3270project/src/edu/gsu/GUI/AccountPanel.java
+++ Airline3270project/src/edu/gsu/GUI/AccountPanel.java
@@ -1,66 +1,86 @@
 package edu.gsu.GUI;
 
 import javax.swing.*;
+
+import edu.gsu.common.Customer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.awt.*;
 import java.awt.event.*;
 
-public class AccountPanel extends JPanel {
+public class AccountPanel extends Application {
 
     private JButton booking;
     private JButton reservation;
     private JButton update;
+    
+    private Customer c1;
+	
+	public AccountPanel (Customer c1) {
+		
+		this.c1 = c1;
+		
+		
+	}
 
-    public AccountPanel(){
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
-        setPreferredSize(new Dimension(300, 200));
-        setBackground(Color.WHITE);
-        setLayout(null);
+    //public AccountPanel() {
 
-        booking = new JButton("Book a Flight");
-        reservation = new JButton("See Reserved Flights");
-        update = new JButton("Update a Flight");
+       // setPreferredSize(new Dimension(300, 200));
+      //  setBackground(Color.WHITE);
+      //  setLayout(null);
 
-        booking.setBounds(20, 20, 260, 45);
-        add(booking);
-        booking.addActionListener(new AccountPanel.ButtonListener());
+      //  booking = new JButton("Book a Flight");
+      //  reservation = new JButton("See Reserved Flights");
+      //  update = new JButton("Update a Flight");
 
-        reservation.setBounds(20, 70, 260, 45);
-        add(reservation);
-        reservation.addActionListener(new AccountPanel.ButtonListener());
+      //  booking.setBounds(20, 20, 260, 45);
+     //   add(booking);
+     //   booking.addActionListener(new AccountPanel.ButtonListener());
 
-        update.setBounds(20, 120, 260, 45);
-        add(update);
-        update.addActionListener(new AccountPanel.ButtonListener());
-    }
+      //  reservation.setBounds(20, 70, 260, 45);
+     //   add(reservation);
+     //   reservation.addActionListener(new AccountPanel.ButtonListener());
 
-    class ButtonListener implements ActionListener {
+     //   update.setBounds(20, 120, 260, 45);
+     //   add(update);
+     //   update.addActionListener(new AccountPanel.ButtonListener());
+    //}
 
-        @Override
-        public void actionPerformed(ActionEvent event) {
+   // class ButtonListener implements ActionListener {
 
-            if(event.getSource() == booking) {
+      // @Override
+      //  public void actionPerformed(ActionEvent event) {
 
-                JFrame frame = new JFrame("BoBo Tour Bookings");
-                frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-                BookingPanel panel = new BookingPanel();
-                frame.getContentPane().add(panel);
-                frame.pack();
-                frame.setVisible(true);
+       //     if(event.getSource() == booking) {
 
-            } else if (event.getSource() == reservation) {
+       //         JFrame frame = new JFrame("BoBo Tour Bookings");
+       ///         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+       //         BookingPanel panel = new BookingPanel();
+       ///         frame.getContentPane().add(panel);
+       //         frame.pack();
+       //         frame.setVisible(true);
 
-                JFrame frame = new JFrame("BoBo Tour Reservations");
-                frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-                ReservationPanel panel = new ReservationPanel();
-                frame.getContentPane().add(panel);
-                frame.pack();
-                frame.setVisible(true);
+       //     } else if (event.getSource() == reservation) {
 
-            } else if (event.getSource() == update) {
+       //         JFrame frame = new JFrame("BoBo Tour Reservations");
+       //        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        //        ReservationPanel panel = new ReservationPanel();
+        //        frame.getContentPane().add(panel);
+       //         frame.pack();
+       //         frame.setVisible(true);
 
-            }
+         //   } else if (event.getSource() == update) {
 
-        }
-    }
+        //    }
+
+      //  }
+  // }
 
 }
